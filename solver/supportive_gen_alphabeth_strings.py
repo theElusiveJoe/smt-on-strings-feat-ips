a5bab05a32c49b7c790bccd49ed003e5446a0569
f <lol@gmail.com>
2022-07-22 10:44:54 +0300

    поправочка к предыдущему коммиту

--- solver/supportive_gen_alphabeth_strings.py
+++ solver/supportive_gen_alphabeth_strings.py
@@ -6,7 +6,7 @@ alpha_3 = ''.join(random.choices(['A', 'B', 'C'], weights=[1,1,0.2], k=strlen))
 alpha_6 = ''.join(random.choices(['A', 'B', 'C', 'D', 'E', 'F'], weights=[1,1,1,1,1,1], k=strlen))
 print(alpha_2, alpha_3, alpha_6)
 
-with open('formula_generator/alphabet_strings.py', 'w+') as dst:
+with open('solver/alphabet_strings.py', 'w+') as dst:
     dst.write(f'alpha_2="{alpha_2}"\n')
     dst.write(f'alpha_3="{alpha_3}"\n')
     dst.write(f'alpha_6="{alpha_6}"\n')
