78d4a069e2e2c9cd61211f50a8f64d4cec95a479
f <lol@gmail.com>
2022-07-25 03:25:00 +0300

    lia игнорирует отрицательные литералы и бесконечно генерирует имена переменных-заглушек и еще теперь выдает адекватный вывод в check_sat()

--- solver/lia.py
+++ solver/lia.py
@@ -1,12 +1,25 @@
-from subprocess import run
+import subprocess
 from .structures import *
 
+def generate_ints():
+    for i in  ['Pineapple', 'Cherry', 'Avocado', 'Orange', 'Peach', 'Pineberry',
+                       'Tamarind', 'Yuzu', 'Lemon', 'Jackfruit', 'Coconut', 'Kumquat', 'Apricot',
+                       'Banana', 'Cucumber', 'Fig', 'Pear', 'Blackberry', 'Lime', 'Soursop',
+                       'Watermelon', 'Raspberry', 'Barsik', 'Papaya', 'Guava', 'Mango', 'Kiwi',
+                       'Passionfruit', 'Durian', 'Grape', 'Starfruit', 'Strawberry', 'Quince',
+                       'Dragonfruit', 'Blueberry', 'Mulberry', 'Gooseberry', 'Plum', 'Apple', 'Persimmon']:
+        yield i
+    
+    i = -1
+    while True:
+        i+=1
+        yield f'var_name_{i}'
 
 class Lia_Formula():
     """
     self.variables нужен для удобного состаления ассертов
     self.atom_to_lia и self.string_to_lia - для мемоизации
-    и получения мнтерпретации в рамках теории lia
+    и получения интерпретации в рамках теории lia
     """
 
     # почему мемоизация пишется без буквы "р"?...
@@ -14,12 +27,7 @@ class Lia_Formula():
     def __init__(self, formula: Formula):
         self.formula = formula
 
-        self.fruits = ['Pineapple', 'Cherry', 'Avocado', 'Orange', 'Peach', 'Pineberry',
-                       'Tamarind', 'Yuzu', 'Lemon', 'Jackfruit', 'Coconut', 'Kumquat', 'Apricot',
-                       'Banana', 'Cucumber', 'Fig', 'Pear', 'Blackberry', 'Lime', 'Soursop',
-                       'Watermelon', 'Raspberry', 'Barsik', 'Papaya', 'Guava', 'Mango', 'Kiwi',
-                       'Passionfruit', 'Durian', 'Grape', 'Starfruit', 'Strawberry', 'Quince',
-                       'Dragonfruit', 'Blueberry', 'Mulberry', 'Gooseberry', 'Plum', 'Apple', 'Persimmon']
+        self.varnames_generator = generate_ints()
 
         self.atom_to_lia = {}
         self.string_to_lia = {}
@@ -29,9 +37,20 @@ class Lia_Formula():
             if atom not in self.atom_to_lia:
                 self.atom_interpretation_in_lia(atom)
 
+    def __str__(self):
+        s = '###LIA_FORMULA###\n'
+        s += 'FAKE STRINGS:\n    '
+        s += ''.join([f'\n    {y} <- {x}' if 'str.re' in x.stype else '' for x,
+                     y in self.string_to_lia.items()])
+        s += '\nATOMS:\n    '
+        s += '\n    '.join([f'{y} <- {x}' for x,
+                           y in self.atom_to_lia.items()])
+        s += '\n\n'
+        return s
+
     def atom_interpretation_in_lia(self, atom: Atom):
         if atom in self.atom_to_lia:
-            return self.atom_to_lia[atom]
+            return
 
         sign = {'=': '=', 'str.contains': '>='}[atom.ltype]
         lia_string1 = self.string_interpretation_in_lia(atom.my_string1)
@@ -60,7 +79,7 @@ class Lia_Formula():
             self.string_to_lia[my_string] = my_string.var_name
             return my_string.var_name
 
-        name = self.fruits.pop()
+        name = next(self.varnames_generator)
         for mss in my_string.replace_strs:
             self.string_interpretation_in_lia(mss)
         self.string_to_lia[my_string] = name
@@ -70,42 +89,34 @@ class Lia_Formula():
     def clause_interpretation_in_lia(self, clause: Clause):
         literals_interpretations = [self.literal_interpretation_in_lia(
             literal) for literal in clause.literals]
-        if len(literals_interpretations) > 1:
-            t = f'(or {" ". join(literals_interpretations)})'
-        else:
-            t = literals_interpretations[0]
+        literals_interpretations = list(filter(None, literals_interpretations))
+        if not literals_interpretations:
+            return
+        t = f'(or {" ". join(literals_interpretations)})'
         return f'{t}'
 
     def literal_interpretation_in_lia(self, literal: Literal):
         if literal.negation:
-            return f'(not {self.atom_to_lia[literal.atom]})'
+            return 
+            # return f'(not {self.atom_to_lia[literal.atom]})'
         return self.atom_to_lia[literal.atom]
-
-    def __str__(self):
-        s = '###LIA_FORMULA###\n'
-        s += 'FAKE STRINGS:\n    '
-        s += ''.join([f'\n    {y} <- {x}' if 'str.re' in x.stype else '' for x,
-                     y in self.string_to_lia.items()])
-        s += '\nATOMS:\n    '
-        s += '\n    '.join([f'{y} <- {x}' for x,
-                           y in self.atom_to_lia.items()])
-        s += '\n\n'
-        return s
 
     def check_sat(self):
         smt2_strings = ['(set-logic QF_LIA)']
         for var_name in self.variables:
             smt2_strings.append(f'(declare-fun {var_name} () Int)')
             smt2_strings.append(f'(assert (>= {var_name} 0))')
-        smt2_strings.append('------------------')
-        for x, y in self.string_to_lia.items():
-            if x.stype == 'str.replace' or x.stype == 'str.replace_all':
-                smt2_strings.append(f'(assert (= {y} {x}))')
-        smt2_strings.append('------------------')
+        # for x, y in self.string_to_lia.items():
+            # if x.stype == 'str.replace' or x.stype == 'str.replace_all':
+            # smt2_strings.append(f'(assert (= {y} {x}))')
+        smt2_strings.append(';------------------')
+
         for clause in self.formula.clauses:
-            smt2_strings.append(
-                f'(assert {self.clause_interpretation_in_lia(clause)})')
-        smt2_strings.append('------------------')
+            clause_interpret = self.clause_interpretation_in_lia(clause)
+            if clause_interpret:
+                smt2_strings.append(
+                    f'(assert {clause_interpret})')
+        smt2_strings.append(';------------------')
         smt2_strings.append('(check-sat)')
         # smt2_strings.append('(get-model)')
 
@@ -116,5 +127,5 @@ class Lia_Formula():
                     f.write(s+'\n')
         except:
             raise Exception('Ошибка при создании/открытии временного файла')
-        
-        return bool(run(['z3', '-smt2', temp]).returncode)
+
+        return not 'unsat' in str(subprocess.check_output(['z3', '-smt2', temp]), encoding='utf-8')
